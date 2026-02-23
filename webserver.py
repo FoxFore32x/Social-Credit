047e2f86a56626d6021937b3e15951f2538cc9b3
FoxFore32x <39179215+FoxFore32x@users.noreply.github.com>
2026-02-22 19:54:36 -0800

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,14 @@
+from flask import flask
+from threading import Thread
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Hello, World!"
+    
+def run():
+    app.run(host='o.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
